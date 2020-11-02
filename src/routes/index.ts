4b985bf44a5170e47e31b4b712aae0d8ef9b6bf7
amaralc <amaral.calil@gmail.com>
2020-11-02 12:41:50 -0300

    FEATURE / Converte rota get em post para teste

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,17 @@
 import { Router } from 'express';
 
 const routes = Router();
+
+/** Escuta método get na rota raiz (/) e responde com objeto json */
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
 export default routes;
