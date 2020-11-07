16351ef88252d0533428aa716d51ee52a09e8bc3
amaralc <amaral.calil@gmail.com>
2020-11-07 12:47:44 -0300

    FEATURE / Passa regras de negocio para service

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,8 +4,9 @@ import { Router } from 'express';
  * parseIso: converte String para objeto Date nativo do JavaScript
  * startOfHour: pega data e coloque minuto, segundo, milisegundos como zero
  */
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 /** Cria roteador de agendamentos */
 const appointmentsRouter = Router();
@@ -24,35 +25,33 @@ appointmentsRouter.get('/', (request, response) => {
 
 /** Escuta método post na rota raiz (/) e responde com objeto json */
 appointmentsRouter.post('/', (request, response) => {
-  /** Busca provider e date de dentro do corpo da requisicao */
-  const { provider, date } = request.body;
+  try {
+    /** Busca provider e date de dentro do corpo da requisicao */
+    const { provider, date } = request.body;
 
-  /** Ajusta formato da data */
-  const parsedDate = startOfHour(parseISO(date));
+    /** Ajusta formato da data */
+    const parsedDate = parseISO(date);
 
-  /**
-   * Avalia se existe agendamento no mesmo horario enviado no corpo da
-   * requisicao e retorna o primeiro appointment encontrado
-   */
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate,
-  );
+    /** Instancia serviço */
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  /** Se encontrou appointment, retorna erro */
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    /** Executa serviço (cria appointment) */
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    /** Devolve resposta à requisição */
+    return response.json(appointment);
+
+    /** Caso o serviço retorne erro */
+  } catch (error) {
+    /**
+     * Retorna mensagem de erro definida no serviço */
+    return response.status(400).json({ error: error.message });
   }
-
-  /** Adiciona agendamento à lista de agendamentos */
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  /** Adiciona appointment criado na lista de appointments */
-  return response.json(appointment);
 });
 
 /** Exporta roteador de agendamentos */
