16f5f186e5080c09a65f94e5d687b096b0291a68
amaralc <amaral.calil@gmail.com>
2020-11-14 19:40:32 -0300

    FEATURE / Importa rota de autenticacao para dentro das rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 /** Cria roteador */
 const routes = Router();
@@ -9,5 +10,7 @@ const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 /** Usa usersRouter para qualquer rota que indicia com /users */
 routes.use('/users', usersRouter);
+/** Usa sessionsRouters para qualquer rota que indicia com /sessions */
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
