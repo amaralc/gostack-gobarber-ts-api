e8dbed88ee0dea96ee264e638e76f7429842a2c5
amaralc <amaral.calil@gmail.com>
2020-11-14 21:20:44 -0300

    FEATURE / Cria middleware de autenticacao

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,53 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+/** Avalia se usuario esta autenticado */
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  /** Salva header de autenticacao de dentro do header da requisicao */
+  const authHeader = request.headers.authorization;
+
+  /** Se nao encontrar o header de autenticacao, retorna erro */
+  if (!authHeader) {
+    throw new Error('JWT token is missing');
+  }
+
+  /**
+   * Formato de desestruturacao: const [type, token]
+   * Se nao vamos utilizar uma das informacoes podemos passar: const [, token]
+   */
+  const [, token] = authHeader.split(' ');
+
+  /** Tenta decodificar token e cria tratativa de erro */
+  try {
+    /** Decodifica token */
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    /** Desestrutura 'decoded' e força tipo da variavel decoded */
+    const { sub } = decoded as TokenPayload;
+
+    /**
+     * Define valor de propriedade 'user' criada dentro do tipo Request
+     * Vide src/@types/express.d.ts
+     */
+    request.user = {
+      id: sub,
+    };
+
+    /** Direciona usuario proxima funcao */
+    return next();
+  } catch (error) {
+    throw new Error('Invalid JWT token');
+  }
+}
