ca1fc90b30abc3b2e2924c2dae74886094468d59
amaralc <amaral.calil@gmail.com>
2020-11-21 22:10:00 -0300

    FEATURE / Cria appointment controller e chama nas rotas de appointment

---
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+/**
+ * Importa metodos de date-fns
+ * parseIso: converte String para objeto Date nativo do JavaScript
+ * startOfHour: pega data e coloque minuto, segundo, milisegundos como zero
+ */
+import { parseISO } from 'date-fns';
+import { container } from 'tsyringe';
+
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+export default class AppointmentController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    /** Busca provider_id e date de dentro do corpo da requisicao */
+    const { provider_id, date } = request.body;
+
+    /** Ajusta formato da data */
+    const parsedDate = parseISO(date);
+
+    /** Instancia serviço utilizando container de injecao de dependencias */
+    const createAppointment = container.resolve(CreateAppointmentService);
+
+    /** Executa serviço (cria appointment) */
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    /** Devolve resposta à requisição */
+    return response.json(appointment);
+  }
+}
--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
-/**
- * Importa metodos de date-fns
- * parseIso: converte String para objeto Date nativo do JavaScript
- * startOfHour: pega data e coloque minuto, segundo, milisegundos como zero
- */
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 /** Importa middleware de autenticacao */
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
+/** Importa controller */
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+
 /** Cria roteador de agendamentos */
 const appointmentsRouter = Router();
+
+/** Instancia controller */
+const appointmentsController = new AppointmentsController();
 
 /**
  * Aplica middleware para todas as rotas abaixo desta linha
@@ -33,25 +31,7 @@ appointmentsRouter.use(ensureAuthenticated);
 // });
 
 /** Escuta método post na rota raiz (/) e responde com objeto json */
-appointmentsRouter.post('/', async (request, response) => {
-  /** Busca provider_id e date de dentro do corpo da requisicao */
-  const { provider_id, date } = request.body;
-
-  /** Ajusta formato da data */
-  const parsedDate = parseISO(date);
-
-  /** Instancia serviço utilizando container de injecao de dependencias */
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  /** Executa serviço (cria appointment) */
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  /** Devolve resposta à requisição */
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 /** Exporta roteador de agendamentos */
 export default appointmentsRouter;
