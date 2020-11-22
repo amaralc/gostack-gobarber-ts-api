032a08d116aaebd393ad4ac4d765ef284fcfe57c
amaralc <amaral.calil@gmail.com>
2020-11-22 11:51:32 -0300

    FEATURE / Cria testes para avaliar criacao de usuario

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,50 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    /** Instancia repositorio */
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    /** Instancia servico passando repositorio como dependencia */
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    /** Executa serviço */
+    const user = await createUser.execute({
+      name: 'User One',
+      email: 'user1@email.com',
+      password: '123456',
+    });
+
+    /** Avalia resultado */
+    expect(user).toHaveProperty('id');
+    expect(user.email).toBe('user1@email.com');
+  });
+
+  it('should not be able to create a new user with same email of another user', async () => {
+    /** Instancia repositorio */
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    /** Instancia servico passando repositorio como dependencia */
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    /** Executa serviço */
+    await createUser.execute({
+      name: 'User One',
+      email: 'user1@email.com',
+      password: '123456',
+    });
+
+    /** Executa serviço novamente para gerar erro */
+    const userWithRepeatedEmail = createUser.execute({
+      name: 'User One',
+      email: 'user1@email.com',
+      password: '123456',
+    });
+
+    /** Avalia resultado */
+    expect(userWithRepeatedEmail).rejects.toBeInstanceOf(AppError);
+  });
+});
