07f38d68b78cf7aed8e1da966180321feb50f09e
amaralc <amaral.calil@gmail.com>
2020-11-22 13:47:08 -0300

    FEATURE / Adiciona testes para verificar envio de erros

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -6,6 +6,27 @@ import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
+  it('should not be able to authenticate with non existing user', async () => {
+    /** Instancia fakes */
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    /** Instancia servico passando fakes como dependencia */
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    /** Executa serviço */
+    const authenticateWithNonExistingUser = authenticateUser.execute({
+      email: 'user1@email.com',
+      password: '123456',
+    });
+
+    /** Avalia resultado */
+    expect(authenticateWithNonExistingUser).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should be able to authenticate the user', async () => {
     /** Instancia fakes */
     const fakeUsersRepository = new FakeUsersRepository();
@@ -39,28 +60,35 @@ describe('AuthenticateUser', () => {
     expect(response.user).toEqual(user);
   });
 
-  // it('should not be able to create a new user with same email of another user', async () => {
-  //   /** Instancia repositorio */
-  //   const fakeUsersRepository = new FakeUsersRepository();
+  it('should not be able to authenticate user with wrong password', async () => {
+    /** Instancia fakes */
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-  //   /** Instancia servico passando repositorio como dependencia */
-  //   const createUser = new CreateUserService(fakeUsersRepository);
+    /** Instancia servicos passando fakes como dependencias */
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
-  //   /** Executa serviço */
-  //   await createUser.execute({
-  //     name: 'User One',
-  //     email: 'user1@email.com',
-  //     password: '123456',
-  //   });
+    /** Cria usuario */
+    await createUser.execute({
+      name: 'User One',
+      email: 'user1@email.com',
+      password: '123456',
+    });
 
-  //   /** Executa serviço novamente para gerar erro */
-  //   const userWithRepeatedEmail = createUser.execute({
-  //     name: 'User One',
-  //     email: 'user1@email.com',
-  //     password: '123456',
-  //   });
+    /** Executa serviço de autenticacao */
+    const authenticateWithWrongPassword = authenticateUser.execute({
+      email: 'user1@email.com',
+      password: 'wrong-password',
+    });
 
-  //   /** Avalia resultado */
-  //   expect(userWithRepeatedEmail).rejects.toBeInstanceOf(AppError);
-  // });
+    /** Avalia resultado */
+    expect(authenticateWithWrongPassword).rejects.toBeInstanceOf(AppError);
+  });
 });
