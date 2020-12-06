bc86eb1b99aea3f78dfeaad6a80531ee09fae14f
amaralc <amaral.calil@gmail.com>
2020-12-05 21:41:55 -0300

    FEATURE / Cria IParseMailTemplateDTO

---
+++ src/shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO.ts
@@ -0,0 +1,10 @@
+/** Cria interface com quantidade variavel de elementos */
+interface ITemplateVariables {
+  /** Define propriedade que tem chave do tipo string */
+  [key: string]: string | number;
+}
+
+export default interface IParseMailTemplateDTO {
+  template: string;
+  variables: ITemplateVariables;
+}
