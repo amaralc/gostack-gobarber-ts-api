2b69cce1c7c27bd456d45b0b64ae2143286263b6
amaralc <amaral.calil@gmail.com>
2020-12-06 18:51:16 -0300

    FEATURE / Chama routes de profile na raiz das rotas

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import appointmentsRouter from '@modules/appointments/infra/http/routes/appointm
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 /** Cria roteador */
 const routes = Router();
@@ -15,5 +16,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 /** Usa passwordRouter para qualquer rota que indicia com /password */
 routes.use('/password', passwordRouter);
+/** Usa profileRouter para qualquer rota que indicia com /profile */
+routes.use('/profile', profileRouter);
 
 export default routes;
