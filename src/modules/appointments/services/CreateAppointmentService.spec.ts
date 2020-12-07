76084481ea9468e75d04ab2437fdf865801627fa
amaralc <amaral.calil@gmail.com>
2020-12-07 17:31:23 -0300

    FEATURE / Adiciona user_id ao teste de createAppointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -20,31 +20,40 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appointment', async () => {
+    /** Define variaveis locais para teste */
+    const providerId = 'provider-id';
+    const userId = 'user-id';
+
     /** Executa serviço */
     const appointment = await createAppointment.execute({
       date: new Date(),
-      provider_id: '123123123',
+      provider_id: providerId,
+      user_id: userId,
     });
 
     /** Avalia resultado */
-    expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123123123');
+    await expect(appointment).toHaveProperty('id');
+    await expect(appointment.provider_id).toBe(providerId);
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    /** Define parametro */
+    /** Define variaveis locais para teste */
+    const providerId = 'provider-id';
+    const userId = 'user-id';
     const appointmentDate = new Date();
 
     /** Executa serviço */
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123123',
+      provider_id: providerId,
+      user_id: userId,
     });
 
     /** Executa serviço novamente para gerar erro */
     const appointmentOnSameTime = createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123123',
+      provider_id: providerId,
+      user_id: userId,
     });
 
     /** Avalia resultado */
