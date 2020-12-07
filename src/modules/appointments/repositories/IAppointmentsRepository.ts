a428f9833acd6530131acc4d186ab33f02421288
amaralc <amaral.calil@gmail.com>
2020-12-07 14:15:41 -0300

    FEATURE / Cria metodo na interface do repositorio

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,13 +1,19 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   /** Define tipagem do metodo create */
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   /** Define tipo de parametro da interface */
   findByDate(date: Date): Promise<Appointment | undefined>;
+  /** Define tipo do metodo de listagem no mes */
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+  /** Define tipo do metodo de listagem no dia */
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
   ): Promise<Appointment[]>;
 }
