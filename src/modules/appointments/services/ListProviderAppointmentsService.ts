9b72cc51a38d898496604335c4af4c89486f437f
amaralc <amaral.calil@gmail.com>
2020-12-13 11:29:34 -0300

    FEATURE / Salva e busca dados do cache no service de listagem de appointments

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,7 +1,8 @@
 import { injectable, inject } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   provider_id: string;
@@ -15,6 +16,9 @@ export default class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -23,6 +27,11 @@ export default class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
+    /** Busca dado no cache */
+    const cacheData = await this.cacheProvider.recover('asd');
+
+    console.log(cacheData);
+
     /** Busca appointments */
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       {
@@ -32,6 +41,9 @@ export default class ListProviderAppointmentsService {
         year,
       },
     );
+
+    /** Salva dados no cache */
+    // await this.cacheProvider.save('asd', 'asd');
 
     /** Retorna appointments */
     return appointments;
